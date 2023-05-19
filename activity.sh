54772384ad323e9321d1af70d27cce560f7a11c0
tee8z <tee8z@protonmail.com>
2023-05-19 12:43:20 -0400

    fixing a path issue in the activity.sh file

--- activity.sh
+++ activity.sh
@@ -2,6 +2,7 @@
 
 for i in {1..6}; do 
     for i in {1..6}; do 
-        source /home/pi/mutinynet_script/send_payments.sh &
+        source /home/pi/mutinynet_scripts/send_payments.sh
     done
 done
+
